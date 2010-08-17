e0030ad79c227cdc74360baef75b88e4c101d2d8
Mark Mayfield <mark.mayfield@cityvoice.com>
2010-08-17 15:33:19 -0500

    Some initial work with named scopes.

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,4 +1,12 @@
 class ContactForm < ActiveRecord::Base
   belongs_to :campaign
-  has_many :submissions  
+  has_many :submissions
+
+
+  # INSTANCE BEHAVIOR
+
+  def number_of_submissions_between(start_date = Date.today - 1.day, end_date = Date.today - 1.day)
+    self.submissions.between(start_date, end_date).count
+  end
+
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :contact_form
+
+  named_scope :between, lambda { |start_date, end_date| {:conditions => ['created_at between ? AND ?', start_date.to_time.utc.at_beginning_of_day, end_date.to_time.utc.end_of_day]} }
+
 end
