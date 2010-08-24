bad0ec8806b78c69c6412ce44b6f64ea6fedc1c5
Mark Mayfield <mark.mayfield@cityvoice.com>
2010-08-24 13:56:43 -0500

    Fixed the bug.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :contact_form
 
-  named_scope :between, lambda { |start_date, end_date| {:conditions => ['created_at between ? AND ?', start_date.to_time.utc.at_beginning_of_day, end_date.to_time.utc.end_of_day]} }
+  named_scope :between, lambda { |start_date, end_date| {:conditions => ['time_of_submission between ? AND ?', start_date.to_time.utc.at_beginning_of_day, end_date.to_time.utc.end_of_day]} }
 
 end
