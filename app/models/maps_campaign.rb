81281e2a2e982e938ce699b86ccff216fbfd34ca
bniemietz <ben@cityvoice.com>
2010-09-02 11:04:35 -0500

    Commented out Campaign Pulls

--- app/models/maps_campaign.rb
+++ app/models/maps_campaign.rb
@@ -7,7 +7,7 @@ class MapsCampaign < ActiveRecord::Base
 
   GOOGLE_MAPS_API_KEY = 'ABQIAAAAU2DhWAoQ76ku3zRokt1DnRQX-pfkEHFxdgQJJn1KX_braIcbexTk-gFyApGHhSC0zwacV0-kZeHAzg'
   CHART_COLORS = ["66ccff", "669966", "666666", "cc3366", "ff6633", "ffff33", "000000"]
-  
+
   # INSTANCE BEHAVIOR
 
   def number_of_visits_by_date
@@ -38,6 +38,71 @@ class MapsCampaign < ActiveRecord::Base
     if self.map_keywords != nil
       count = 6
       ranking_count = 0
+      while count >= 0
+        data_date = month_date - count.months
+        data_time_start = Date.new(data_date.year, data_date.month, 1)
+        data_time_end = Date.new(data_date.year, data_date.month, data_date::class.civil(data_date.year, data_date.month, -1).day)
+        kcount = self.map_keywords.count
+        j = 0
+        while j < kcount
+          info = self.map_keywords[j].map_rankings.find(:all, :conditions => ['ranking_date between ? AND ?', data_time_start, data_time_end])
+          if info.last != nil
+            rank = 0
+            rank = info.last.google_rank if info.last.google_rank != 1000
+            rankings[j].push(rank)
+            ranking_count = rank if rank > ranking_count
+          else
+            rank = 0
+            rankings[j].push(rank)
+            ranking_count = rank if rank > ranking_count
+          end
+          j += 1
+        end
+      end
+      rankings.each do |ranking|
+        ranking.each_index { |index| ranking[index] = (ranking_count - ranking[index]) if ranking[index] != 0 }
+      end
+
+      days_url = ''
+      kcount = self.map_keywords.count
+      if kcount != 0
+        chart_size = width.to_s + 'x' + height.to_s
+        GoogleChart::LineChart.new(chart_size, 'Google Local Map Keyword Rankings', false) do |bc|
+          k = 0
+          while k < kcount
+            bc.data self.map_keywords[k].descriptor + ' (' + rankings[k].last.to_s + ')', rankings[k], CHART_COLORS[k]
+            k += 1
+          end
+          bc.axis :x, :labels => klabels, :font_size => 10
+          bc.axis :y, :range => [(ranking_count + 1), 1], :font_size => 10
+          bc.fill(:background, :solid, {:color => '65432100'})
+          bc.fill(:chart, :solid, {:color => '65432100'})
+          bc.show_legend = true
+          days_url = bc.to_escaped_url
+        end
+      end
+      return days_url
+
+    end
+  end
+
+  def google_map_data_counts_graph(height = 150, width = 700)
+    height = 300 if height > 300
+    width = 1000 if width > 1000
+    month_date = Date.today
+    rankings = Array.new
+    citations = Array.new
+    user_contents = Array.new
+    coupons = Array.new
+    reviews = Array.new
+    klabels = Array.new
+
+    self.map_keywords.each do |map_keyword|
+      rankings << Array.new
+    end
+
+    if self.map_keywords != nil
+      count = 6
       counts_count = 0
       while count >= 0
         data_date = month_date - count.months
@@ -56,16 +121,7 @@ class MapsCampaign < ActiveRecord::Base
             conts = info.last.google_user_content_count if info.last.google_user_content_count != 0
             coups = info.last.google_coupon_count if info.last.google_coupon_count != 0
             revs = info.last.google_review_count if info.last.google_review_count != 0
-            rank = 0
-            rank = info.last.google_rank if info.last.google_rank != 1000
-            rankings[j].push(rank)
-            ranking_count = rank if rank > ranking_count
-          else
-            rank = 0
-            rankings[j].push(rank)
-            ranking_count = rank if rank > ranking_count
           end
-
           j += 1
         end
         klabels.push((data_date.month.to_s + '/' + data_date.year.to_s[2..3]))
@@ -79,34 +135,60 @@ class MapsCampaign < ActiveRecord::Base
         counts_count = coups if coups > counts_count
         count -= 1
       end
-      rankings.each do |ranking|
-        ranking.each_index {|index| ranking[index] = (ranking_count - ranking[index])  if ranking[index] != 0}
-      end
-
-      days_url = ''
-      kcount = self.map_keywords.count
-      if kcount != 0
-        chart_size = width.to_s + 'x' + height.to_s
-        GoogleChart::LineChart.new(chart_size, 'Google Local Map Keyword Rankings', false) do |bc|
-          k = 0            
-          while k < kcount
-            bc.data self.map_keywords[k].descriptor + ' (' + rankings[k].last.to_s + ')', rankings[k], CHART_COLORS[k]
-            k += 1
-          end
-          bc.axis :x, :labels => klabels, :font_size => 10
-          bc.axis :y, :range => [(ranking_count + 1), 1], :font_size => 10
-          bc.fill(:background, :solid, {:color => '65432100'})
-          bc.fill(:chart, :solid, {:color => '65432100'})
-          bc.show_legend = true
-          days_url = bc.to_escaped_url
-        end
-      end
-      return days_url
-      
     end
+    data_url = ''
+    chart_size = width + 'x' + height
+    GoogleChart::LineChart.new(chart_size, 'Google Local Map Data Counts', false) do |bc|
+      bc.data 'Reviews', reviews, CHART_COLORS[0]
+      bc.data 'Citations', citations, CHART_COLORS[1]
+      bc.data 'User Content', user_contents, CHART_COLORS[2]
+      bc.data 'Coupons', coupons, CHART_COLORS[3]
+      bc.axis :x, :labels => klabels, :font_size => 10
+      bc.axis :y, :range => [0, counts_count], :font_size => 10
+      bc.fill(:background, :solid, {:color => '65432100'})
+      bc.fill(:chart, :solid, {:color => '65432100'})
+      bc.show_legend = true
+      data_url = bc.to_escaped_url
+    end
+    return data_url
   end
 
-  def google_map_data_counts_graph(height = 150, width = 700)
-   
+  def google_map_website_clicks_graph(height = 150, width = 900)
+       height = 300 if height > 300
+    width = 1000 if width > 1000
+       month_date = Date.today
+           slabels = Array.new
+       searches = Array.new
+       search_max = 0
+    m = 11
+              while m > 0
+                data_date = month_date - count.months
+                        data_time_start = Date.new(data_date.year, data_date.month, 1)
+                        data_time_end = Date.new(data_date.year, data_date.month, data_date::class.civil(data_date.year, data_date.month, -1).day)
+                       info = self.websites.first.map_visits_between(data_time_start, data_time_end)
+
+                if info != nil
+                  searches.push(info.map_searches)
+                  search_max = info.map_searches if info.map_searches > search_max
+                else
+                  searches.push(0)
+                end
+                slabels.push((data_time_start.month.to_s + '/' + data_time_start.year.to_s[2..3]))
+                m -= 1
+              end
+              search_url = ''
+       chart_size = width + 'x' + height
+
+              GoogleChart::LineChart.new(chart_size, 'Google Local Map Website Clicks', false) do |bc|
+                bc.data 'Clicks on Website', searches, CHART_COLORS[0]
+                bc.axis :x, :labels => slabels, :font_size => 10
+                bc.axis :y, :range => [0, search_max], :font_size => 10
+                bc.fill(:background, :solid, {:color => '65432100'})
+                bc.fill(:chart, :solid, {:color => '65432100'})
+                bc.show_legend = false
+                search_url = bc.to_escaped_url
+              end
+    return search_url
+
   end
 end
