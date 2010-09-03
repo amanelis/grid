040aec19e77f6afc1820454d9150c4b941b997c1
Paul Singh <paul@resultsjunkies.com>
2010-09-03 08:56:38 -0400

    One more Notifier fix.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,4 +1,13 @@
 class Notifier < ActionMailer::Base
-  
+  default_url_options[:host] = APP_CONFIG[:host] 
+
+  def form_submission(submission)
+    subject       "New CityVoice Lead!"  
+    from          "CityVoice <no-reply@cityvoice.com>"
+    #recipients    [email1, email2]
+    recipients    #TODO
+    body          :submission => submission
+    sent_on       Time.now
+  end
 
 end
