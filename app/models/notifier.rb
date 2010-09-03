aff961c55fbc855030b9d53b5f87779dbe996954
markleem <mark.mayfield@cityvoice.com>
2010-09-03 09:22:43 -0500

    Added to development addresses to the form submission.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,11 @@ class Notifier < ActionMailer::Base
   default_url_options[:host] = APP_CONFIG[:host] 
 
   def form_submission(submission)
-    recipients    submission.contact_form.forwarding_email.split(/, \s*/)
+    the_recipients = submission.contact_form.forwarding_email.split(/, \s*/)
+    the_recipients << "dev@cityvoice.com"
+    the_recipients << "forms@cityvoice.com"
+
+    recipients    the_recipients
     subject       "New CityVoice Lead!"  
     from          "CityVoice <no-reply@cityvoice.com>"
     body          :submission => submission
