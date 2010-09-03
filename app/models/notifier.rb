ad5f1585b8af0974403ed9253ba5f240a69d5b61
markleem <mark.mayfield@cityvoice.com>
2010-09-03 09:05:31 -0500

    Added email recipients to notifier's form submission method.

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,10 +2,9 @@ class Notifier < ActionMailer::Base
   default_url_options[:host] = APP_CONFIG[:host] 
 
   def form_submission(submission)
+    recipients    submission.contact_form.forwarding_email.split(/, \s*/)
     subject       "New CityVoice Lead!"  
     from          "CityVoice <no-reply@cityvoice.com>"
-    #recipients    [email1, email2]
-    recipients    #TODO
     body          :submission => submission
     sent_on       Time.now
   end
