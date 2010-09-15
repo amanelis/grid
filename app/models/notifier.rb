471d7bd8549012e149882bd1b7050749c0646b2a
bniemietz <ben@cityvoice.com>
2010-09-15 10:08:10 -0500

    Changed the from email on submissions

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -6,10 +6,11 @@ class Notifier < ActionMailer::Base
     the_recipients = submission.contact_form.forwarding_email.split(/, \s*/)
     the_recipients << "dev@cityvoice.com"
     the_recipients << "forms@cityvoice.com"
+    the_sender = submission.name + ' <' + submission.from_email + '>'
 
     recipients    the_recipients
     subject       "New CityVoice Lead!"  
-    from          "CityVoice <no-reply@cityvoice.com>"
+    from          the_sender
     body          :submission => submission
     sent_on       Time.now
   end
