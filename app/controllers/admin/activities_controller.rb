661c2177c719ac646621b23458effda8201b9c16
Paul Singh <paul@resultsjunkies.com>
2010-09-24 08:48:26 -0500

    Adding in the first bit of JS for the activity auto-update but it's disabled until I can fully test out all the JS stuff.

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -4,9 +4,8 @@ class Admin::ActivitiesController < ApplicationController
   
   def index
     @user = current_user
-    @activities = Account.leads_in_previous_hours(Time.at(params[:after].to_i))
-    
-    @activities = @activities.paginate(:page => params[:page])
+    @activities = Account.leads_in_previous_hours(Time.at(params[:after].to_i + 1))
+    @activities = @activities.paginate(:page => (params[:page] || 1))
   end
   
   def update
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Commenting this all out until we have a little more volume
+// and/or I find a better way to test this out. [PS]
+//$(function () {  
+  //if ($('.activity').length > 0) {  
+  //  setTimeout(updateActivity, 10000);  
+  //}  
+//});  
+  
+function updateActivity() {
+  var after = $('.activity_item:first').attr('id')
+  $.getScript('activities.js?after=' + after);
+  if ($('.activity_notice').length == 0) {
+	setTimeout(updateActivity, 60000);  
+  }
+}
