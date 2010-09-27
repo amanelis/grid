221d9188b2c54dde5f042ea3c5564f00101f38a1
markleem <mark.mayfield@cityvoice.com>
2010-09-27 16:23:52 -0500

    Added descending order to the previous hours named scope.

--- app/models/call.rb
+++ app/models/call.rb
@@ -31,7 +31,7 @@ class Call < ActiveRecord::Base
 
   named_scope :between, lambda { |start_date, end_date| {:conditions => ['call_start between ? AND ?', start_date.to_time.utc.at_beginning_of_day, end_date.to_time.utc.end_of_day]} }
   named_scope :snapshot, lambda { |start_datetime, duration| {:conditions => ['call_start between ? AND ?', start_datetime.utc, start_datetime.utc + duration.minutes]} }
-  named_scope :previous_hours, lambda { |*args| {:conditions => ['call_start > ?', (args.first || nil)]} }
+  named_scope :previous_hours, lambda { |*args| {:conditions => ['call_start > ?', (args.first || nil)], :order => 'call_start DESC'} }
 
   has_attached_file :recording,
                     :storage => :s3,
