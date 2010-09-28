f99d938e4e5e950ee914b964de25db04cdc41179
markleem <mark.mayfield@cityvoice.com>
2010-09-28 10:29:39 -0500

    Added helper methods to activity to determine the activity type.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,5 +2,13 @@ class Activity < ActiveRecord::Base
   belongs_to :activity_type, :polymorphic => true
 
   named_scope :previous_hours, lambda { |*args| {:conditions => ['timestamp > ?', (args.first || nil)], :order => 'timestamp DESC'} }
+  
+  def is_call?
+    self.activity_type.instance_of?(Call)
+  end
+  
+  def is_submission?
+    self.activity_type.instance_of?(Submission)
+  end
 
 end
