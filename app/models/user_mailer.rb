537e688dfb7c2f8c21dbd3310a5458f64f0e9be2
Paul Singh <paul@resultsjunkies.com>
2010-10-06 17:06:31 -0500

    Fixed the password reset stuff. It's ugly, but it works.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,14 +1,12 @@
 class UserMailer < ActionMailer::Base
   default_url_options[:host] = APP_CONFIG[:host]
   
-  def forgot_password(sent_at = Time.now)
-    def password_reset_instructions(user)  
-      subject       "Password Reset Instructions"  
-      from          "CityVoice Notifier"  
-      recipients    user.email  
-      sent_on       Time.now  
-      body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
-    end
+  def password_reset_instructions(user)  
+    subject       "Password Reset Instructions"  
+    from          "CityVoice Notifier"  
+    recipients    user.email  
+    sent_on       Time.now  
+    body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
   end
 
 end
