a6dc89dcb65d67b907b48058c413b5304b625a22
markleem <mark.mayfield@cityvoice.com>
2010-10-08 11:01:37 -0500

    Minor tweak to spam filter.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -30,7 +30,7 @@ class Submission < ActiveRecord::Base
   
   def is_spam?
     return true if self.phone_number =~ /123456/
-    return true if self.work_description =~ /http:/
+    return true if self.work_description =~ /http:/i
     return true if self.work_description =~ /search.*engine/i
     return true if self.work_description =~ /internet.* marketing/i
     false
