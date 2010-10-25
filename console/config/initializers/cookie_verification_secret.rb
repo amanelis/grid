7dcb869526a80e144e70821fdc1cbc50bc0d0e85
Alex Baldwin <alexbaldwin@gmail.com>
2010-10-24 19:37:27 -0500

    accidentally created a rails app inside the app

---
+++ console/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '7d9a3a5618928100a01b95cd5573110bdb3a04f5435e61eae318d9e86503cc9f6010847bd12bce72ba566c936ae8156d491d9cd9c56331601b5c8f468e66fc0c';
