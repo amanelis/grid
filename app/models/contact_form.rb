6f10fd9430903881ea360f7d4aad4068cb682e6b
markleem <mark.mayfield@cityvoice.com>
2010-11-02 11:35:08 -0500

    Now using the lead named scope for submissions.

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -6,11 +6,11 @@ class ContactForm < ActiveRecord::Base
   # INSTANCE BEHAVIOR
 
   def number_of_submissions_between(start_date = Date.yesterday, end_date = Date.yesterday)
-    self.submissions.non_spam.between(start_date, end_date).count
+    self.submissions.lead.between(start_date, end_date).count
   end
 
   def number_of_submissions_by_date
-    self.submissions.non_spam.count(:group => "date(time_of_submission)", :order =>"time_of_submission ASC").inject({}) {|data, (key, value)| data[key.to_date] = {:submission => value} ; data}
+    self.submissions.lead.count(:group => "date(time_of_submission)", :order =>"time_of_submission ASC").inject({}) {|data, (key, value)| data[key.to_date] = {:submission => value} ; data}
   end
   
   def inactive?
