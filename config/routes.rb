f130e5deb6219c938a51026749d64a4c98f2d0c3
bniemietz <ben@cityvoice.com>
2010-12-07 17:03:12 -0600

    Fixed Website error

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :keywords
     admin.resources :job_statuses
     admin.resources :websites
-    admin.resources :website_visits
+    admin.resources :website_visits, :member => {:global_visitor => :get}
     admin.resources :searches, :only => :index
   end
 
