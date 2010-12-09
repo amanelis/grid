bd737fa2e8c150b77ab644ac3899f8df41dbed92
markleem <mark.mayfield@cityvoice.com>
2010-12-09 14:48:05 -0600

    Added conversion rates to the campaign and website views.

--- app/models/website_visit.rb
+++ app/models/website_visit.rb
@@ -1,7 +1,7 @@
 class WebsiteVisit < ActiveRecord::Base
   belongs_to :website
 
-  named_scope :bounce, :conditions => ['actions = ? AND time_total < ?', 1, 30]
+  named_scope :bounce, :conditions => ['actions = ? AND time_total <= ?', 1, 15]
   named_scope :referred, :conditions => ['referrer_search IS NOT NULL']
   named_scope :from_google_maps, :conditions => ['referrer_url like ?', '%maps.google.com%']
   named_scope :from_yahoo_maps, :conditions => ['referrer_url like ?', '%local.yahoo.com%']
