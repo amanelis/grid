c88fa3a4e19b1436cef08a8276b00c2538fd299a
bniemietz <ben@cityvoice.com>
2010-12-09 09:08:47 -0600

    updated web_visits hard_update

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -71,7 +71,7 @@ class ApplicationManifest < Moonshine::Manifest::Rails
       cron 'pull_salesforce_campaigns', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} Campaign.pull_salesforce_campaigns", :user => configuration[:user], :minute => 45, :hour => 22
       cron 'update_calls', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} Call.update_calls", :user => configuration[:user], :minute => '*/15', :hour => '*'
       cron 'add_websites', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} Website.add_websites", :user => configuration[:user], :minute => 1, :hour => 23
-      cron 'data_pull_websites_visits', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} WebsiteVisit.data_pull_websites_visits", :user => configuration[:user], :minute => 45, :hour => '*/8'
+      cron 'data_pull_websites_visits', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} WebsiteVisit.data_pull_websites_visits", :user => configuration[:user], :minute => 45, :hour => '*'
       cron 'update_keywords_from_salesforce', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} Keyword.update_keywords_from_salesforce", :user => configuration[:user], :minute => 31, :hour => 22
       cron 'update_keyword_rankings', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} Keyword.update_keyword_rankings", :user => configuration[:user], :minute => 1, :hour => 1
       cron 'update_inbound_links', :command => "/home/deploy/grid/current/script/runner -e #{deploy_stage} SeoCampaign.update_inbound_links", :user => configuration[:user], :minute => 1, :hour => 2
--- app/models/website_visit.rb
+++ app/models/website_visit.rb
@@ -10,11 +10,10 @@ class WebsiteVisit < ActiveRecord::Base
   named_scope :between, lambda { |start_date, end_date| {:conditions => ['time_of_visit between ? AND ?', start_date.to_time_in_current_zone.at_beginning_of_day.utc, end_date.to_time_in_current_zone.end_of_day.utc], :order => 'time_of_visit ASC'} }
   named_scope :for_date, lambda { |date| {:conditions => ['time_of_visit between ? AND ?', date.to_time_in_current_zone.at_beginning_of_day.utc, date.to_time_in_current_zone.end_of_day.utc]} }
   named_scope :for_visitor, lambda { |visitor| {:conditions => ['visitor_id = ?', visitor]} }
-
-
+  
   # CLASS BEHAVIOR
 
-  def self.data_pull_websites_visits(hard_update = false, start_date = (Date.today - 3.days), end_date = Date.yesterday, verbose = false)
+  def self.data_pull_websites_visits(hard_update = false, start_date = (Date.today - 2.days), end_date = Date.yesterday, verbose = false)
     job_status = JobStatus.create(:name => "WebsiteVisit.data_pull_websites_visits")
     exception = nil
     sites = Website.find(:all, :conditions => ['is_active = ? OR is_active is null', 1])
@@ -36,7 +35,7 @@ class WebsiteVisit < ActiveRecord::Base
     Account.cache_results_for_accounts
   end
 
-  def self.update_website_visits(website_id, start = Date.today - 3.days, fend = Date.yesterday, verbose = false)
+  def self.update_website_visits(website_id, start = Date.today - 2.days, fend = Date.yesterday, verbose = false)
     exception = nil
     website = Website.find(website_id)
     if website.present?
@@ -103,13 +102,110 @@ class WebsiteVisit < ActiveRecord::Base
     exception
   end
 
-  def self.hard_update_website_visits(website_id, start = Date.today - 3.days, fend = Date.yesterday)
-    #Eventually make a difference between the two....hard will overwrite data or something
+  def self.hard_update_website_visits(website_id, start = Date.today - 2.days, fend = Date.yesterday)
+    exception = nil
+    website = Website.find(website_id)
+    if website.present?
+      type = 'visitors-list&visitor-details'
+
+      time_begin = start.to_time.utc.at_beginning_of_day
+      time_end = fend.to_time.utc.end_of_day
+      count = ((time_end - time_begin).round)/86400
+
+      pull_date = start
+
+      while count != 0
+        url_dates = pull_date.to_s
+        mainurl = "http://stats.cityvoice.com.re.getclicky.com/api/stats/4?site_id=" + website.site_id + "&sitekey=" + website.sitekey + "&date=" + url_dates + "&type=" + type + "&output=json&limit=10000"
+        response = HTTParty.get(mainurl).first
+        if response["error"].present? && response["error"] == "Invalid sitekey."
+          website.is_active = false
+          website.save!
+        else
+          if response["dates"].present?
+            dates = response["dates"].first
+            visits = dates["items"]
+            visits.each do |visit|
+              begin
+                nurl = WebsiteVisit.find_by_session_id_and_website_id(visit["session_id"], website.id)
+                if nurl.present?
+                  nurl.actions = visit["actions"]
+                  nurl.clicky_url = visit["clicky_url"]
+                  nurl.latitude = visit["latitude"]
+                  nurl.longitude = visit["longitude"]
+                  nurl.language = visit["language"]
+                  nurl.screen_resolution = visit["screen_resolution"]
+                  nurl.time = visit["time"]
+                  nurl.time_pretty = visit["time_pretty"]
+                  nurl.time_total = visit["time_total"]
+                  nurl.ip_address = visit["ip_address"]
+                  nurl.geolocation = visit["geolocation"]
+                  nurl.javascript = visit["javascript"]
+                  nurl.web_browser = visit["web_browser"]
+                  nurl.operating_system = visit["operating_system"]
+                  nurl.referrer_url = visit["referrer_url"]
+                  nurl.referrer_search = visit["referrer_search"]
+                  nurl.hostname = visit["hostname"]
+                  nurl.organization = visit["organization"]
+                  nurl.campaign = visit["campaign"]
+                  nurl.goals = visit["goals"]
+                  nurl.custom = visit["custom"]
+                  nurl.landing_page = visit["landing_page"]
+                  nurl.referrer_type = visit["referrer_type"]
+                  nurl.visitor_id = visit["uid"]
+                  nurl.time_of_visit = Time.at(visit["time"].to_i)
+                  nurl.save
+                else
+                  nurl = WebsiteVisit.find_or_create_by_session_id_and_website_id(:session_id => visit["session_id"],
+                                                                                :website_id => website.id,
+                                                                                :actions => visit["actions"],
+                                                                                :clicky_url => visit["clicky_url"],
+                                                                                :latitude => visit["latitude"],
+                                                                                :longitude => visit["longitude"],
+                                                                                :language => visit["language"],
+                                                                                :screen_resolution => visit["screen_resolution"],
+                                                                                :time => visit["time"],
+                                                                                :time_pretty => visit["time_pretty"],
+                                                                                :time_total => visit["time_total"],
+                                                                                :ip_address => visit["ip_address"],
+                                                                                :geolocation => visit["geolocation"],
+                                                                                :javascript => visit["javascript"],
+                                                                                :web_browser => visit["web_browser"],
+                                                                                :operating_system => visit["operating_system"],
+                                                                                :referrer_url => visit["referrer_url"],
+                                                                                :referrer_search => visit["referrer_search"],
+                                                                                :hostname => visit["hostname"],
+                                                                                :organization => visit["organization"],
+                                                                                :campaign => visit["campaign"],
+                                                                                :goals => visit["goals"],
+                                                                                :custom => visit["custom"],
+                                                                                :landing_page => visit["landing_page"],
+                                                                                :referrer_type => visit["referrer_type"],
+                                                                                :visitor_id => visit["uid"],
+                                                                                :time_of_visit => Time.at(visit["time"].to_i))
+                end
+                puts "Saved visit from #{nurl.geolocation}" if verbose
+              rescue Exception => ex
+                exception = ex
+                next
+              end
+            end
+          end
+        end
+        pull_date += 1.day
+        count -= 1
+      end
+    end
+    exception
   end
 
   # INSTANCE BEHAVIOR
   def all_visits_from_visitor()
     self.class.find_all_by_visitor_id(self.visitor_id)
+  end
+  
+  def possible_calls()
+    
   end
   
 end
