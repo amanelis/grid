624118ce4a4edb4c5429ac91cf3b7453453ce195
Alex Manelis <amanelis@gmail.com>
2010-12-20 11:17:07 -0600

    Fixed small assignment error

--- app/controllers/admin/notifiers_controller.rb
+++ app/controllers/admin/notifiers_controller.rb
@@ -5,8 +5,7 @@ class Admin::NotifiersController < ApplicationController
     if params[:id].blank?
       render :text => "Error on report"
     else
-      @account_report_data = Account.find(params[:id])
-      @account_report_data.previous_days_report_data
+      @account_report_data = Account.find(params[:id]).previous_days_report_data
     end
   end
 end
