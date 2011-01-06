13bddf9fcb78c40e141ddd947b765432ef927586
markleem <mark.mayfield@cityvoice.com>
2011-01-06 15:01:45 -0600

    Minor tweaks to user role behavior.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,20 +11,20 @@ class User < ActiveRecord::Base
     UserMailer.deliver_password_reset_instructions(self)  
   end
   
-  def acquainted_with_group_account(group_account)
-    return true is self.admin?
+  def acquainted_with_group_account?(group_account)
+    return true if self.admin?
     self.roles.account_manager.any? { |role| role.role_type.group_account == group_account }
   end
 
-  def acquainted_with_account(account)
-    return true is self.admin?
-    self.roles.account_manager.any? { |role| role.role_type.group_account == account.group_account }
+  def acquainted_with_account?(account)
+    return true if self.admin?
+    return true if self.roles.account_manager.any? { |role| role.role_type.group_account == account.group_account }
     self.roles.account_user.any? { |role| role.role_type.account == account }
   end
 
-  def acquainted_with_campaign(campaign)
-    return true is self.admin?
-    self.roles.account_manager.any? { |role| role.role_type.group_account == campaign.account.group_account }
+  def acquainted_with_campaign?(campaign)
+    return true if self.admin?
+    return true if self.roles.account_manager.any? { |role| role.role_type.group_account == campaign.account.group_account }
     self.roles.account_user.any? { |role| role.role_type.account == campaign.account }
   end
 
