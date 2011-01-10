9ab7a54e1676e341c6c79df36be2e4fbfbe5faa7
bniemietz <ben@cityvoice.com>
2011-01-10 10:58:26 -0600

    made a change on twilio response ;

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -10,9 +10,9 @@ class PhoneNumbersController < ApplicationController
   
   def connect
     @r = Twilio::Response.new
-    @r.append(Twilio::Say.new("For quality purposes, your call will be recorded ", :voice => "woman"))
+    @r.append(Twilio::Say.new("For quality purposes, your call will be recorded ", :voice => "woman", :loop => 1))
+    @r.append(Twilio::Dial.new("2105389224"))
     @r.append(Twilio::Record.new())
-    @r.append(Twilio::Dial.new("2105389224", :timeLimit => "45"))
 
     puts @r.respond
     render :text => @r.respond
