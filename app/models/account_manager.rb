bd74895a489b50c58d1cb12f5a98cf69454727dc
markleem <mark.mayfield@cityvoice.com>
2011-01-13 12:10:50 -0600

    Added some validation to the new roles.

--- app/models/account_manager.rb
+++ app/models/account_manager.rb
@@ -3,5 +3,20 @@ class AccountManager < ActiveRecord::Base
   
   belongs_to :group_account
   has_many :accounts
+
+  validate :unique_role?
   
+  def unique?
+    self.role.user.roles.account_manager.to_a.none? { |role| role.role_type.group_account == self.group_account }
+  end
+  
+  
+  private
+  
+  def unique_role?
+    unless unique?
+      errors.add_to_base("An account manager role already exists for this user and group account")
+    end
+  end
+ 
 end
--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -3,4 +3,19 @@ class AccountUser < ActiveRecord::Base
   
   belongs_to :account
   
+  validate :unique_role?
+  
+  def unique?
+    self.role.user.roles.account_user.to_a.none? { |role| role.role_type.account == self.account }
+  end
+  
+  
+  private
+  
+  def unique_role?
+    unless unique?
+      errors.add_to_base("An account user role already exists for this user and account")
+    end
+  end
+  
 end
