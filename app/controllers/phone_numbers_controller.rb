33ef1401573fd62d14322ee110183831db8ae8fc
bniemietz <ben@cityvoice.com>
2011-01-14 14:03:03 -0600

    Twilio Testing2

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -4,13 +4,14 @@ class PhoneNumbersController < ApplicationController
     @phone_number = PhoneNumber.find(params[:id])
     
     @r = Twilio::Response.new
-    @r.append(Twilio::Dial.new("2105389216")
+    #@r.append(Twilio::Dial.new("2105389216")
     @r.append(Twilio::Say.new("For quality purposes, your call will be recorded ", :voice => "woman", :loop => 1))
+    @r.append(Twilio::Redirect.new("2105389216"))
     #@r.append(Twilio::Dial.new(@phone_number.forward_to))
     #@r.append(Twilio::Record.new(:playBeep => "false")) if @phone_number.record_calls
     puts @r.respond
-      #text_block = "Request: #{@r}\nResponse: #{@r.respond}\ncampaign.name: #{@phone_number.campaign.name}\nname: #{@phone_number.name}\ninboundno: #{@phone_number.inboundno}\ncmpid: #{@phone_number.cmpid}\nforward_to: #{@phone_number.forward_to}\ntwilio_id: #{@phone_number.twilio_id}"
-      #render :text => text_block
+    #text_block = "Request: #{@r.respond}\nResponse: #{@r.respond}\ncampaign.name: #{@phone_number.campaign.name}\nname: #{@phone_number.name}\ninboundno: #{@phone_number.inboundno}\ncmpid: #{@phone_number.cmpid}\nforward_to: #{@phone_number.forward_to}\ntwilio_id: #{@phone_number.twilio_id}"
+    render :text => @r.respond
   end
   
   def collect
