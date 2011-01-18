8e95c19f948da28f91982470bbf9425f7033d8a0
Alex Manelis <amanelis@gmail.com>
2011-01-18 10:09:45 -0600

    Changed RJS miliseconds

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+/*
 $(function() {
-  setTimeout(updateActivity, 3000);
+  setTimeout(updateActivity, 10000);
 });
 
 function updateActivity() {
   $.getScript("/keywords.js")
-  setTimeout(updateActivity, 3000);
+  setTimeout(updateActivity, 10000);
 }
+*/
 
 
