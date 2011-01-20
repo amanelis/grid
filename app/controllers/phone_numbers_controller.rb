a7bf6a33a95e32f017c9c2c2145dc27d40cae080
bniemietz <ben@cityvoice.com>
2011-01-20 09:57:45 -0600

    Updated PhoneNumber.Collect in controller

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -22,22 +22,22 @@ class PhoneNumbersController < ApplicationController
         ##Try to create the number
       else
         call = @phone_number.calls.build
-        call.forwardno = params[:Called]
-        call.caller_name = params[:CallerName]
-        call.inboundno = params[:From]
-        call.call_id = params[:CallSid]
-        call.call_status = params[:CallStatus]
-        call.caller_city = params[:CallerCity]
-        call.caller_state = params[:CallerState]
-        call.caller_zip = params[:CallerZip]
-        call.caller_country = params[:CallerCountry]
-        twilio_call = call.get_twilio_call(params[:CallSid])
-        call.call_start = twilio_call["start_time"]
-        call.call_start = twilio_call["end_time"]
-        call.price = twilio_call["price"]
+        call.forwardno = params["Called"].gsub("+", "")
+        call.caller_name = params["CallerName"]
+        call.inboundno = params["From"].gsub("+", "")
+        call.call_id = params["CallSid"]
+        call.call_status = params["CallStatus"]
+        call.caller_city = params["CallerCity"]
+        call.caller_state = params["CallerState"]
+        call.caller_zipcode = params["CallerZip"]
+        call.caller_country = params["CallerCountry"]
+        twilio_call = Call.get_twilio_call(params["CallSid"])
+        call.call_start = Time.parse(twilio_call["start_time"])
+        call.call_end = Time.parse(twilio_call["end_time"])
+        call.cost = twilio_call["price"]
         call.save!
-        Call.fetch_twilio_call_recording(params[:CallSid])
-        #Call.send_later(:fetch_twilio_call_recording, params[:CallSid]) if call.save!
+        Call.fetch_twilio_recording(params["CallSid"])
+        #Call.send_later(:fetch_twilio_recording, params["CallSid"]) if call.save!
         head 200
       end
     rescue
