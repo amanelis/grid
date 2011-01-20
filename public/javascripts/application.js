24f8cf9ff38a9c978b26eb3fb4649d49766c4ad1
Alex Manelis <amanelis@gmail.com>
2011-01-20 17:37:52 -0600

    Installed facebox for light windows, simple javascript stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-/*
-$(function() {
-  setTimeout(updateActivity, 10000);
-});
-
-function updateActivity() {
-  $.getScript("/keywords.js")
-  setTimeout(updateActivity, 10000);
-}
-*/
+// LOAD Facebox
+jQuery(document).ready(function($) {
+	$('a[rel*=facebox]').facebox() 
+})
 
 
