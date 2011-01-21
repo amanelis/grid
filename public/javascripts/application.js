58430bd1d10482cce4b0c9b1ede9f69194868082
Alex Manelis <amanelis@gmail.com>
2011-01-21 10:09:24 -0600

    Fixed charts, facebox interferes with it

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-// LOAD Facebox
+// LOAD Facebox, this will kill highcharts
+/*
 jQuery(document).ready(function($) {
 	$('a[rel*=facebox]').facebox() 
 })
+*/
 
 
