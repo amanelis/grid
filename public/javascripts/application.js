979e011255ee67437e5b37b58d19f80dd93b9ec3
Alex Manelis <amanelis@gmail.com>
2011-01-27 16:11:05 -0600

    jQuery links added for exporting account report

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,6 @@
 
 function refresh_accounts() {
   jQuery.facebox.close();
-  //$('#loading').html('<p><img src="images/facebox/loading.gif" /></p>');
   $('#loading').html('<h3><font color="green">Loading accounts... This could take about 5 minutes depending on your connection, please be patient.</font></h3>');
   window.location = "/accounts/refresh_accounts"
 }
@@ -13,6 +12,12 @@ function export_accounts() {
   window.location = "/accounts/export"
 }
 
+function export_client_report(id) {
+	jQuery.facebox.close();
+	url = "/accounts/" + id + "/report/client.pdf"
+	window.location = url
+}
+
 function confirm_export_accounts() {
   var message = '<center>This will export account data to a CSV file, continue?</center><br><center><a href="#" onClick="export_accounts();">Yes</a> or <a href="#" onClick="jQuery.facebox.close();">No</a></center>';
   jQuery.facebox(message);
@@ -20,6 +25,11 @@ function confirm_export_accounts() {
 
 function confirm_refresh_accounts() {
   var message = '<center>Refresh accounts from salesforce?<br>This will take about 5 minutes...</center><br><center><a href="#" onClick="refresh_accounts();">Yes</a> or <a href="#" onClick="jQuery.facebox.close();">No</a></center>';
+  jQuery.facebox(message);
+}
+
+function confirm_export_report(id) {
+	var message = '<center>This will export a PDF client report for previous month.</center><br><center><a href="#" onClick="export_client_report('+id+');">Yes</a> or <a href="#" onClick="jQuery.facebox.close();">No</a></center>';
   jQuery.facebox(message);
 }
 
