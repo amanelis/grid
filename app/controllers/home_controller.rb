484532deaac22a732d2a7f13e87b36e073c46337
Alex Manelis <amanelis@gmail.com>
2011-01-28 16:37:20 -0600

    Added metrics to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,21 @@
 class HomeController < ApplicationController
+  caches_page :dashboard
+  
   def index
     @user = current_user
   end
   
-  def dashboard
-    @user = current_user    
+  def dashboard  
+    authorize! :read, Account
+    @user = current_user
+    if @user.admin?
+      @accounts           = Account.all.count
+      @active_accounts    = Account.active.count
+      @inactive_accounts  = Account.inactive.count
+      @reseller_accounts  = Account.reseller.count
+    end
+    
+    
+    
   end
 end
