5c646afdbc668c3c5cc7b2c0792c845a783d0715
bniemietz <ben@cityvoice.com>
2011-02-07 10:07:54 -0600

    Fixed any Campaign.find_by_name!

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -45,7 +45,17 @@ class ContactForm < ActiveRecord::Base
   end
   
   def get_form_snippet
-    
+    #<script type="text/javascript">var host = (("https:" == document.location.protocol) ? "https://secure." : "http://");document.write(unescape("%3Cscript src='" + host + "wufoo.com/scripts/embed/form.js' type='text/javascript'%3E%3C/script%3E"));</script>
+
+    #<script type="text/javascript">
+   # var z7x4a3 = new WufooForm();
+    #z7x4a3.initialize({
+    #'userName':'cityvoice', 
+    #'formHash':'z7x4a3', 
+    #'autoResize':true,
+    #'height':'600'});
+    #z7x4a3.display();
+    #</script>
   end
   
   
