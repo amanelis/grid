434508e30808f96ce15d84f9ca52c475fda5ac81
markleem <mark.mayfield@cityvoice.com>
2011-02-09 10:55:30 -0600

    New behaviors to determine a user's manipulable group accounts, accounts, and campaigns.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ActiveRecord::Base
     UserMailer.deliver_password_reset_instructions(self)  
   end
   
+  def group_user?
+    self.group_users.present?
+  end
+  
   def account_manager?
     self.account_managers.present?
   end
@@ -25,29 +29,54 @@ class User < ActiveRecord::Base
   
   def acquainted_with_group_account?(group_account)
     return true if self.admin?
-    self.account_managers.any? { |account_manager| account_manager.group_account == group_account }
+    self.group_users.any? { |group_user| group_user.group_account == group_account }
+  end
+
+  def can_manipulate_group_account?(group_account)
+    return true if self.admin?
+    self.group_users.any? { |group_user| group_user.manipulator? && group_user.group_account == group_account }
   end
 
   def acquainted_with_account?(account)
     return true if self.admin?
-    return true if self.account_managers.any? { |account_manager| account_manager.group_account == account.group_account }
+    return true if self.group_users.any? { |group_user| group_user.group_account == account.group_account }
     self.account_users.any? { |account_user| account_user.account == account }
+  end
+
+  def can_manipulate_account?(account)
+    return true if self.admin?
+    self.group_users.any? { |group_user| group_user.manipulator? && group_user.group_account == account.group_account }
   end
 
   def acquainted_with_campaign?(campaign)
     return true if self.admin?
-    return true if self.account_managers.any? { |account_manager| account_manager.group_account == campaign.account.group_account }
+    return true if self.group_users.any? { |group_user| group_user.group_account == campaign.account.group_account }
     self.account_users.any? { |account_user| account_user.account == campaign.account }
+  end
+  
+  def can_manipulate_campaign?(campaign)
+    return true if self.admin?
+    self.group_users.any? { |group_user| group_user.manipulator? && group_user.group_account == campaign.account.group_account }
   end
   
   def acquainted_group_accounts
     return GroupAccount.all if self.admin?
-    self.account_managers.collect { |account_manager| account_manager.group_account }
+    self.group_users.collect { |group_user| group_user.group_account }
+  end
+  
+  def manipulable_group_accounts
+    return GroupAccount.all if self.admin?
+    self.group_users.select(&:manipulator?).collect { |group_user| group_user.group_account }
   end
   
   def acquainted_accounts
     return Account.all if self.admin?
-    (self.account_managers.collect { |account_manager| account_manager.group_account.accounts } << self.account_users.collect { |account_user| account_user.account }).flatten.uniq
+    (self.group_users.collect { |group_user| group_user.group_account.accounts } << self.account_users.collect { |account_user| account_user.account }).flatten.uniq
+  end
+
+  def manipulable_accounts
+    return Account.all if self.admin?
+    self.group_users.select(&:manipulator?).collect { |group_user| group_user.group_account.accounts }.flatten
   end
 
   def acquainted_campaigns
