540183421546baff746ecb9b3308ed96982e0b07
Matthew Johnston <warmwaffles@gmail.com>
2011-02-14 14:50:54 -0600

    Added jquery audio support with a flash fallback

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -39,3 +39,37 @@ function show_report(id) {
   $.facebox(message);
   $("#report_message").load(url);
 }
+
+function playSound(url) {
+  var message;
+
+  // If they have no support for audio
+  if(! Modernizr.audio || ! Modernizr.audio.mp3){
+    message = '<center><div class="mp3_player" id="mp3_player">'+
+      '<object type="application/x-shockwave-flash" data="/player.swf" height="24" width="290">'+
+       '<param name="movie" value="/player.swf" />'+
+       '<param name="FlashVars" value="righticonhover=0x222222&leftbg=0xAAAAAA&text=0x222222&slider=0x222222&bg=0xEEEEEE&rightbg=0xAAAAAA&loader=0xEEEEEE&rightbghover=0xEEEEEE&track=0xEEEEEE&lefticon=0x222222&border=0x333333&righticon=0x222222&loop=no&autostart=no&animation=no&soundFile='+url+'" />'+
+       '<param name="quality" value="high" />'+
+       '<param name="menu" value="true" />'+
+       '<param name="wmode" value="transparent" />'+
+    '</object>'+
+    '</div></center>';
+  } else {
+    // They support audio
+    message = "<center><audio id=\"mp3_player\" src=\""+url+"\" preload=\"auto\"></audio></center>";
+  }
+  // show the audio
+  $.facebox(message);
+
+  if(Modernizr.audio) {
+    // initialize the audiojs
+    audiojs.events.ready(function(){
+      audiojs.createAll();
+    });
+  }
+
+  // When facebox closes, remove the mp3_player element
+  $(document).bind('close.facebox', function() {
+    $("#mp3_player").remove();
+  });
+}
