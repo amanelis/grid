8a912f50a7eb5012f9ad818fb6478c656335f120
Alex Baldwin <alexbaldwin@gmail.com>
2011-02-15 14:20:47 -0600

    search results and admin link changed

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -60,12 +60,12 @@ class AccountsController < ApplicationController
     datepicker account_path(params[:id])
     
     @daily_total_leads_graph = HighChart.new('graph') do |f|
-      f.title({:text=>"Total Daily Leads"})  
-      f.y_axis({:title=> {:text=> 'Daily Leads'}, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
-      f.x_axis(:categories => ((@start_date)..(@end_date)).to_a , :labels=>{:rotation=>-45 , :align => 'right'})
+      f.title(:text => false)  
+      f.y_axis({:title=> false, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
+      f.x_axis(:type => 'datetime', :labels=> {:align => 'center'}, :tickWidth => 0)
       f.legend(:enabled => false)
       
-      f.options[:chart][:defaultSeriesType] = "line"
+      f.options[:chart][:defaultSeriesType] = "area"
       f.series(:name=> 'Leads', :data => (@start_date..@end_date).inject([]) { |leads, date| leads << @managed_campaigns.sum { |campaign| campaign.number_of_total_leads_between(date, date) } })
     end
     
