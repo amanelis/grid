806b5ef631d91dc05df3aec000be2b02604aded8
Alex Baldwin <alexbaldwin@gmail.com>
2011-02-16 11:24:01 -0600

    graph modified to awesomeness

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -62,11 +62,11 @@ class AccountsController < ApplicationController
     @daily_total_leads_graph = HighChart.new('graph') do |f|
       f.title(:text => false)  
       f.y_axis({:title=> false, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
-      f.x_axis(:type => 'datetime', :labels=> {:align => 'center'}, :tickWidth => 0)
+      f.x_axis(:type => 'datetime', :tickInterval => 7 * 24 * 3600 * 1000, :dateTimeLabelFormats =>{:week => "%b %e"})
       f.legend(:enabled => false)
       
       f.options[:chart][:defaultSeriesType] = "area"
-      f.series(:name=> 'Leads', :fillOpacity => '.3', :data => (@start_date..@end_date).inject([]) { |leads, date| leads << @managed_campaigns.sum { |campaign| campaign.number_of_total_leads_between(date, date) } })
+      f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000 , :data => (@start_date..@end_date).inject([]) { |leads, date| leads << @managed_campaigns.sum { |campaign| campaign.number_of_total_leads_between(date, date) } })
     end
     
     @campaign_summary_graph = HighChart.new('graph') do |f|
