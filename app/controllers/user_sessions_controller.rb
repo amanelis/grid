abdf934fcf189e1c321f27411c507bfbaca37f83
Alex Manelis <amanelis@gmail.com>
2011-02-23 09:58:41 -0600

    Removed retarted flash message on campaigns and stoped flash on user sign in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,6 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
       redirect_back_or_default dashboard_url
     else
       flash[:error] = "Username/password was invalied."
