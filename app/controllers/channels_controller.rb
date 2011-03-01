ea61b004ba1e9006c5b7c73cae0caaf365ba4cc2
Alex Manelis <amanelis@gmail.com>
2011-03-01 15:13:34 -0600

    changed a permission on channels controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,7 +2,6 @@ class ChannelsController < ApplicationController
   inherit_resources
   load_resource :basic_channel, :through => :account
   load_and_authorize_resource :account
-  #load_and_authorize_resource :channel, :through => :account, :except => [:new, :create]
   belongs_to :account
   
   def show
