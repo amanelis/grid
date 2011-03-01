ee8a87f751a790ba307e99e6a324733c9faba044
Alex Manelis <amanelis@gmail.com>
2011-03-01 14:28:17 -0600

    Fixed small mismatch on channel authorization

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,7 +2,7 @@ class ChannelsController < ApplicationController
   inherit_resources
   load_resource :basic_channel, :through => :account
   load_and_authorize_resource :account
-  load_and_authorize_resource :campaign, :through => :account, :except => [:new, :create]
+  #load_and_authorize_resource :channel, :through => :account, :except => [:new, :create]
   belongs_to :account
   
   def show
