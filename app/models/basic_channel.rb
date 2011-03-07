0cc6fb28aa1bfdc4393bdbe5c27a7bc68d1a61a2
markleem <mark.mayfield@cityvoice.com>
2011-03-07 11:22:14 -0600

    Forgot to delete one.

--- app/models/basic_channel.rb
+++
@@ -1,9 +0,0 @@
-class BasicChannel < ActiveRecord::Base
-  belongs_to :account
-  has_many :basic_campaigns
-  has_many :campaigns, :class_name => "BasicCampaign", :foreign_key => "basic_channel_id"
-  
-  validates_presence_of :name
-  validates_uniqueness_of :name, :case_sensitive => false, :scope => "account_id"
-
-end
