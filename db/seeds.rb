8fac5d091e7438eaadc4b5c2d2585ff4552b81c7
Matthew Johnston <warmwaffles@gmail.com>
2011-03-08 08:44:21 -0600

    Added a little more help about seeding the database in the admin section, and I added a user seed without admin roles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,9 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#   
+#
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
 User.create!(:email => "admin@cityvoice.com", :password =>"admin", :password_confirmation => "admin", :admin => 1)
+User.create!(:email => "user@cityvoice.com", :password =>"user", :password_confirmation => "user", :admin => 0)
