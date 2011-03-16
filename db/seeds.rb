b0135358a8fbfca06005bf69681b4f1ec1b04cdd
Alex Manelis <amanelis@gmail.com>
2011-03-16 09:46:36 -0500

    Small typo on nav, seed file update cause i nuked my db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-User.create!(:email => "admin@cityvoice.com", :password =>"admin", :password_confirmation => "admin", :admin => 1)
-User.create!(:email => "user@cityvoice.com", :password =>"user", :password_confirmation => "user", :admin => 0)
+User.create!(:email => "admin@cv.com", :password =>"admin",   :password_confirmation => "admin", :admin => 1)
+User.create!(:email => "user@cv.com",  :password =>"user",    :password_confirmation => "user",  :admin => 0)
+User.create!(:email => "guw@cv.com",   :password => "guw123", :password_confirmation => "guw123")
+User.create!(:email => "gur@cv.com",   :password => "gur123", :password_confirmation => "gur123")
+User.create!(:email => "auw@cv.com",   :password => "auw123", :password_confirmation => "auw123")
+User.create!(:email => "aur@cv.com",   :password => "aur123", :password_confirmation => "aur123")
