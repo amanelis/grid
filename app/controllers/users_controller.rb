d5c34ea498461874ef3ee5dfda6a1e47749619f5
Alex Manelis <amanelis@gmail.com>
2011-03-16 17:36:08 -0500

    User type creation under accounts works minus manipulator status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,40 +8,56 @@ class UsersController < ApplicationController
   # We want to only list out the users that the current user can manipulate
   def index
     @current_user = current_user
-    @users = @current_user.manipulable_users
+    @users = @current_user.manipulable_users.compact
   end
   
   def new
     authorize! :create, User
     no_layout
     @user = User.new
+    @account = Account.find(params[:account_id]) if params[:account_id].present?
   end
   
   def create
-    @current_user = current_user
-    @account = Account.find(params[:account_id]) if params[:account_id].present?
-
-    if @current_user.group_user?
-      if @current_user.can_manipulate_account?(@account)
-        
-      else
-        
-      end
-      
-    elsif @current_user.user?
-      
-    elsif @current_user.admin?
-      
-    end
+    @current_user   = current_user
+    @account        = Account.find(params[:account_id]) if params[:account_id].present?
+    @user           = User.new(:email => params[:user][:email], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
+    @group_account  = @account.group_account
+    type            = params[:user][:type]
     
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "You just added a new user!"
-      redirect_back_or_default users_path
     else
       flash.now[:error] = "User was not able to be saved, please try again!"
       render :action => :new
     end
+
+    if type == "1" || type == "2"
+      @group_user                 = GroupUser.new 
+      @group_user.user            = @user
+      @group_user.group_account   = @group_account
+      
+      if type == "1" && @user.can_manipulate_account?(@account)
+        @group_user.manipulator = true
+      end
+      
+      @group_user.save
+      redirect_to users_path
+      
+    elsif type == "3" || type == "4"
+      @account_user         = AccountUser.new
+      @account_user.user    = @user
+      @account_user.account = @account
+      
+      if type == "3" && @user.can_manipulate_account?(@account)
+        @account_user.manipulator = true
+      end
+      
+      @account_user.save
+      redirect_to users_path
+    end
+    
   end
   
   def edit
