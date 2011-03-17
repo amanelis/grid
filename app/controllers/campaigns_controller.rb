d8a3da436cbdf01f6539b12185f6b11e8fd8168b
Alex Manelis <amanelis@gmail.com>
2011-03-17 12:33:50 -0500

    Fixed staging form error

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -25,7 +25,7 @@ class CampaignsController < ApplicationController
       #number = bc.campaign.create_twilio_number(params[:campaign][:area_code], params[:campaign][:name], params[:campaign][:forward_to])
       #form = bc.campaign.create_contact_form('', params[:campaign][:forwarding_email])
       flash[:notice] = "Good job, you just created a campaign!"
-      redirect_to channel_campaign_path(@account, @channel, @account.campaigns.last, :form => form)
+      redirect_to channel_campaign_path(@account, @channel, @account.campaigns.last)
     elsif @channel.channel_type == "sem"
       # Creating a SEM 
     elsif @channel.channel_type == "seo"
