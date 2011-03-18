c584da419542c356a5daf45327243ae2e454bc69
Alex Baldwin <alexbaldwin@gmail.com>
2011-03-18 13:46:24 -0500

    keyword graph 30% implemented

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -37,16 +37,6 @@ class CampaignsController < ApplicationController
   def show
     authorize! :read, @campaign
     datepicker channel_campaign_path(@account, @channel, @campaign)
-    
-    @daily_keyword_ranking_graph = HighChart.new('graph') do |f|
-      f.title(:text => false)  
-      f.y_axis({:title=> false, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
-      f.x_axis(:type => 'datetime', :tickInterval => 7 * 24 * 3600 * 1000, :dateTimeLabelFormats =>{:week => "%b %e"})
-      f.legend(:enabled => false)
-      
-      f.chart(:defaultSeriesType => 'area', :backgroundColor => false)
-      f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => (6))
-    end
   end
   
   def edit
--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -9,6 +9,16 @@ class KeywordsController < ApplicationController
   
   def show
     @rankings = @keyword.keyword_rankings.sort { |a,b| b.created_at <=> a.created_at }
+    
+    @daily_keyword_ranking_graph = HighChart.new('graph') do |f|
+      f.title(:text => false)  
+      f.y_axis({:title=> false, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
+      f.x_axis(:type => 'datetime', :tickInterval => 7 * 24 * 3600 * 1000, :dateTimeLabelFormats =>{:week => "%b %e"})
+      f.legend(:enabled => false)
+      
+      f.chart(:defaultSeriesType => 'area', :backgroundColor => false)
+      f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => ([4,5,6]))
+    end
   end
   
 end
