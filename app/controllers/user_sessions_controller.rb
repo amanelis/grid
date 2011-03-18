9ad89eda853e3458d3176c129bb9041dbd0879b9
Alex Manelis <amanelis@gmail.com>
2011-03-18 13:56:53 -0500

    Abiliy giving me hell, but making progress

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,6 +9,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
+      flash[:notice] = "Login successful!"
       redirect_back_or_default dashboard_url
     else
       flash[:error] = "Username/password was invalid."
