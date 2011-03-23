90a775318a10b442256760a9eae6e66eac968dbc
Alex Manelis <amanelis@gmail.com>
2011-03-23 09:13:58 -0500

    Ooops forgot to delete vendored gem reference

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 
 class ApplicationController < ActionController::Base
   include ExceptionNotification::Notifiable
-  
   helper :all # include all helpers, all the time
   #protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
