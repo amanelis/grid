55efa4fd51a16906ffb165e6b7f0245d9b721ccd
Alex Baldwin <alexbaldwin@gmail.com>
2011-03-25 12:19:35 -0500

    fixed keywords ranking graphs

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -21,14 +21,14 @@ class KeywordsController < ApplicationController
   
       @daily_keyword_ranking_graph = HighChart.new('graph') do |f|
         f.title(:text => false)  
-        f.y_axis({:title=> false, :min => 0, :labels=>{:rotation=>0, :align=>'right'} })
+        f.y_axis({:title=> {:text=> 'Search Engine Rank'}, :min => 0, :labels=>{:align=>'right'} })
         f.x_axis(:type => 'datetime', :tickInterval => 7 * 24 * 3600 * 1000, :dateTimeLabelFormats =>{:week => "%b %e"})
-        f.legend(:enabled => false)
+        f.legend(:enabled => true)
     
-        f.chart(:defaultSeriesType => 'area', :backgroundColor => false)
-        f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_google_ranking_between(@start_date, @end_date))
-        #f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_yahoo_ranking_between(@start_date, @end_date))
-        #f.series(:name=> 'Leads', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_bing_ranking_between(@start_date, @end_date))
+        f.chart(:defaultSeriesType => 'line', :backgroundColor => false)
+        f.series(:name=> 'Google Ranking', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_google_ranking_between(@start_date, @end_date))
+        f.series(:name=> 'Yahoo Ranking', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_yahoo_ranking_between(@start_date, @end_date))
+        #f.series(:name=> 'Bing Ranking', :fillOpacity => '.3', :pointInterval => 24 * 3600 * 1000, :pointStart => @start_date.to_time_in_current_zone.at_beginning_of_day.utc.to_i * 1000, :data => @keyword.daily_most_recent_bing_ranking_between(@start_date, @end_date))
       end
     end
     
