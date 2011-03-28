989c468d5c84896facf08d1a5b52718936530847
Alex Manelis <amanelis@gmail.com>
2011-03-28 13:26:48 -0500

    Login redirect, somehow got removed...

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,7 @@ class UserSessionsController < ApplicationController
   before_filter :require_user, :only => :destroy
   
   def new
-    defined?(current_user_session) ? (redirect_to dashboard_url) : (@user_session = UserSession.new)
+    current_user_session ? (redirect_to dashboard_url) : (@user_session = UserSession.new)
   end
   
   def create
