7a9d564aa70dd3eea1d06d69483cf3ec2f7c1338
Matthew Johnston <warmwaffles@gmail.com>
2011-03-29 10:45:44 -0500

    Leads showing on campaign level now. Now we need someone to filter it for that specific campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,17 +1,17 @@
 class CampaignsController < ApplicationController
   inherit_resources
   load_resource :except => [:create]
-  load_resource :accounts 
+  load_resource :accounts
   load_resource :channels
-  
+
   belongs_to :account
   belongs_to :channel
-  
+
   def new
     authorize! :manipulate_account, @account
     no_layout
   end
-  
+
   def create
     authorize! :manipulate_account, @account
     if @channel.channel_type == "basic"
@@ -21,7 +21,7 @@ class CampaignsController < ApplicationController
       bc.name    = params[:campaign][:name]
       bc.status  = "Active"
 
-      if bc.save && bc.campaign.save 
+      if bc.save && bc.campaign.save
         flash[:notice] = "Good job, you just created a campaign!"
       else
         flash[:error] = "Looks like you might have already named a campaign with a similar name, please try again!"
@@ -31,13 +31,13 @@ class CampaignsController < ApplicationController
       adwords_id    = params[:campaign][:adwords_id]
       landing_page  = params[:campaign][:landing_page]
       rake          = params[:campaign][:rake]
-      
+
       sc = SemCampaign.new
       sc.account  = @account
       sc.channel  = @channel
       sc.name     = name
       sc.rake     = rake
-  
+
       if sc.save && sc.campaign.save && sc.campaign.create_website(website)
         flash[:notice] = "Good job, you just created a campaign!"
       else
@@ -48,13 +48,13 @@ class CampaignsController < ApplicationController
       website   = params[:campaign][:url]
       budget    = params[:campaign][:budget]
       keywords  = params[:campaign][:keyword]
-      
+
       sc = SeoCampaign.new
       sc.account  = @account
       sc.channel  = @channel
       sc.name     = name
       sc.budget   = budget
-      
+
       if sc.save && sc.campaign.save && sc.campaign.create_website(website)
         flash[:notice] = "Good job, you just created a campaign!"
       else
@@ -67,25 +67,26 @@ class CampaignsController < ApplicationController
   def show
     authorize! :read, @campaign
     datepicker channel_campaign_path(@account, @channel, @campaign)
+    @activities = Activity.paginate(:page => (params[:page] || 1), :order => 'timestamp DESC', :per_page => 50)
   end
-  
+
   def edit
     authorize! :manipulate_campaign, @campaign
     render :layout => false
   end
-  
+
   def update
     authorize! :manipulate_campaign, @campaign
     if @campaign.is_basic?
       @campaign.update_attributes(:name => params[:campaign][:name]) ? (flash[:notice] = "Updated!") : (flash[:error] = "Ooops looks like there was an error updating your campaign, try again!")
     elsif @campaign.is_sem?
-      @campaign.update_attributes(:name => params[:campaign][:name]) && @account.adwords_client.update_attributes(:reference_id => params[:campaign][:adwords_id]) && @campaign.campaign_style.update_attributes(:rake => params[:campaign][:rake])  && @campaign.website.update_attributes(:domain => params[:campaign][:landing_page]) ?  (flash[:notice] = "Updated!") : (flash[:error] = "Ooops looks like there was an error updating your campaign, try again!") 
+      @campaign.update_attributes(:name => params[:campaign][:name]) && @account.adwords_client.update_attributes(:reference_id => params[:campaign][:adwords_id]) && @campaign.campaign_style.update_attributes(:rake => params[:campaign][:rake])  && @campaign.website.update_attributes(:domain => params[:campaign][:landing_page]) ?  (flash[:notice] = "Updated!") : (flash[:error] = "Ooops looks like there was an error updating your campaign, try again!")
     elsif @campaign.is_seo?
       @campaign.update_attributes(:name => params[:campaign][:name]) && @campaign.website.update_attributes(:domain => params[:campaign][:url]) && @campaign.campaign_style.update_attributes(:budget => params[:campaign][:budget]) ? (flash[:notice] = "Updated!") : (flash[:error] = "Ooops looks like there was an error updating your campaign, try again!")
     end
     redirect_to account_path(@account)
   end
-  
+
   # We want to change active status not destroy the actual campaign
   def destroy
     authorize! :manipulate_campaign, @campaign
@@ -93,7 +94,7 @@ class CampaignsController < ApplicationController
     flash.now[:error] = "Ooops, there was an error deleting that CAMPAIGN, you might want to try again."
     redirect_to account_path(@account)
   end
-  
+
   def lead_matrix
     @campaign = Campaign.find(params[:id])
     authorize! :lead_matrix, @campaign
@@ -107,8 +108,8 @@ class CampaignsController < ApplicationController
         @minutes_selected = 2
       end
     end
-    
-    if params[:datepicker].blank? 
+
+    if params[:datepicker].blank?
       @date_selected = Date.yesterday
     else
       @campaign = Campaign.find(params[:id])
@@ -121,30 +122,30 @@ class CampaignsController < ApplicationController
       end
     end
   end
-  
+
   def create_new_campaign_contact_form
     authorize! :manipulate_campaign, @campaign
     @campaign = Campaign.find(params[:id])
     if @campaign.present?
-      @form_text = @campaign.create_contact_form(params[:description], 
-                                                 params[:return_url], 
-                                                 params[:forwarding_email], 
-                                                 params[:forwarding_bcc_email], 
-                                                 params[:custom1_text], 
-                                                 params[:custom2_text], 
-                                                 params[:custom3_text], 
-                                                 params[:custom4_text], 
-                                                 params[:need_name], 
-                                                 params[:need_address], 
-                                                 params[:need_phone], 
-                                                 params[:need_email], 
-                                                 params[:work_category], 
-                                                 params[:work_description], 
-                                                 params[:date_requested], 
-                                                 params[:time_requested], 
+      @form_text = @campaign.create_contact_form(params[:description],
+                                                 params[:return_url],
+                                                 params[:forwarding_email],
+                                                 params[:forwarding_bcc_email],
+                                                 params[:custom1_text],
+                                                 params[:custom2_text],
+                                                 params[:custom3_text],
+                                                 params[:custom4_text],
+                                                 params[:need_name],
+                                                 params[:need_address],
+                                                 params[:need_phone],
+                                                 params[:need_email],
+                                                 params[:work_category],
+                                                 params[:work_description],
+                                                 params[:date_requested],
+                                                 params[:time_requested],
                                                  params[:other_information])
       @form = @campaign.contact_forms.last
     end
   end
-  
+
 end
