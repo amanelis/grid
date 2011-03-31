0bb25d0ff1c9bd79bef87e041abba7bcde5c69ec
Alex Manelis <amanelis@gmail.com>
2011-03-31 10:33:45 -0500

    Chapter 2: less bs, clicky working, ginza working, katie is smiling...

--- app/models/website.rb
+++ app/models/website.rb
@@ -279,7 +279,8 @@ class Website < ActiveRecord::Base
       raise
     end
   end
-  
+
+=begin 
   def create_clicky_site
     url = "https://api.getclicky.com/api/account/sites?username=cityvoicesa&password=C1tyv01c3&output=json"
     site_id = sitekey = database_server = admin_sitekey = ''
@@ -302,8 +303,20 @@ class Website < ActiveRecord::Base
       self.save!
       return "Website was created!\nClick Code: <script src=\"http://stats.cityvoice.com/js\" type=\"text/javascript\"></script><script type=\"text/javascript\">citystats.init(#{site_id});</script><noscript><p><img alt=\"CityStats\" width=\"1\" height=\"1\" src=\"http://stats.cityvoice.com/#{site_id}ns.gif\" /></p></noscript>"
     end
-    
   end
+=end
+
+  def create_clicky_site
+     info = HTTParty.get("http://stats.cityvoice.com.re.getclicky.com/api/whitelabel/?auth=de8f1bae61c60eb0&type=site&user_id=134255&domain=#{self.domain}&nickname=#{self.nickname}&dst=usa").parsed_response.split("\n")
+     if info.first == 'OK'
+       self.is_active = true
+       self.site_id = info[1]
+       self.sitekey = info[2]
+       self.database_server = info[3]
+       self.admin_sitekey = info[4]
+       return "Website was created!\nClick Code: <script src=\"http://stats.cityvoice.com/js\" type=\"text/javascript\"></script><script type=\"text/javascript\">citystats.init(#{site_id});</script><noscript><p><img alt=\"CityStats\" width=\"1\" height=\"1\" src=\"http://stats.cityvoice.com/#{site_id}ns.gif\" /></p></noscript>"
+     end
+   end
   
   #TESTING GINZA METHODS
   
