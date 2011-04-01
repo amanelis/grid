98a82073be7a494b61f2096338680f76f0d84209
Alex Manelis <amanelis@gmail.com>
2011-04-01 15:00:37 -0500

    Uhh forgot what I commited

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:create]
-  before_filter :require_user, :only => :destroy
+  before_filter :require_user, :only => [:destroy]
   
   def new
     current_user_session ? (redirect_to dashboard_url) : (@user_session = UserSession.new)
