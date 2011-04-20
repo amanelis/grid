9498ee96d2910e3e489092f7be2cfdeda4e338c4
markleem <mark.mayfield@cityvoice.com>
2011-04-20 14:40:27 -0500

    A few more field validations.

--- app/models/budget_infusion.rb
+++ app/models/budget_infusion.rb
@@ -3,6 +3,7 @@ class BudgetInfusion < ActiveRecord::Base
   
   named_scope :between, lambda { |start_date, end_date| {:conditions => ['commitment_date between ? AND ?', start_date, end_date]} }
   
-  validates_numericality_of :amount, :greater_than_or_equal_to => 0.01, :message => "must be an amount $0.01 or greater"  
+  validates_numericality_of :amount, :greater_than_or_equal_to => 0.01, :message => "must be an amount $0.01 or greater"
+  validates_presence_of :commitment_date
   
 end
--- app/models/budget_setting.rb
+++ app/models/budget_setting.rb
@@ -4,5 +4,6 @@ class BudgetSetting < ActiveRecord::Base
   named_scope :upto, lambda { |date| {:conditions => ['start_date <= ?', date], :order => 'start_date ASC'} }
   
   validates_numericality_of :amount, :greater_than_or_equal_to => 0.0, :message => "must be an amount $0.00 or greater"
+  validates_presence_of :start_date
   
 end
