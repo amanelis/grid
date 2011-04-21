b845ebb81a8e0acaa53b4042f9294e874fa04589
Matthew Johnston <warmwaffles@gmail.com>
2011-04-21 10:05:40 -0500

    We've been getting get requests for form submissions. If it is a get request, I just make it go to the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,22 @@ ActionController::Routing::Routes.draw do |map|
   map.logout              "/logout",                                      :controller => "user_sessions",     :action => :destroy
   map.register            "/register",                                    :controller => "users",             :action => :new
   map.refresh_accounts    "/accounts/refresh_accounts",                   :controller => "accounts",          :action => :refresh_accounts
-                                                                          
-  # Twilio routing                                                                 
+
+  # Twilio routing
   map.connect_number_api  "/api/v1/calls/:encoded_number/connect",        :controller => "api/v1/calls",       :action => :connect
   map.collect_number_api  "/api/v1/calls/:encoded_number/complete",       :controller => "api/v1/calls",       :action => :complete
-                                                                                              
+
   map.get_html            "/api/v1/forms/:form_id/get_html",              :controller => "api/v1/forms",       :action => :get_html
   map.get_iframe          "/api/v1/forms/:form_id/get_iframe",            :controller => "api/v1/forms",       :action => :get_iframe
   map.thank_you           "/api/v1/forms/:form_id/thank_you",             :controller => "api/v1/forms",       :action => :thank_you
   map.form_submit         "/api/v1/forms/submit",                         :controller => "api/v1/forms",       :action => :submission, :conditions => { :method => :post }
-                                                                                              
+
   map.form_submit_old     "/submission",                                  :controller => "api/v1/forms",       :action => :submission, :conditions => { :method => :post }
+  map.form_submit_old     "/submission",                                  :controller => "home",               :action => 'index',     :conditions => { :method => :get }
   map.form_thank_old      "/contact_forms/:id/thank_you",                 :controller => "api/v1/forms",       :action => :thank_you
   map.get_numners         "/accounts/:account_id/channels/:channel_id/campaigns/:campaign_id/phone_numbers/new", :controller => "phone_numbers", :action => :new, :conditions => {:method => [:get, :post]}
   # CUSTOM ROUTES
-  
+
   map.resources :accounts do |account|
     account.resources :users
     account.resources :channels, :name_prefix => "" do |channel|
@@ -33,23 +34,23 @@ ActionController::Routing::Routes.draw do |map|
       end
     end
   end
-  
+
   map.namespace(:api) do |api|
     api.resources :calls
     api.resources :forms
   end
-  
+
   map.resources   :user_sessions
   map.resources   :contact_forms,   :member => {:thank_you => :get, :get_html => :get, :get_iframe => :get}
   map.resources   :website_visits,  :member => {:global_visitor => :get}
   map.resources   :calls,           :member => {:collect => :post}
   map.resources   :searches,        :activities, :keywords, :job_statuses, :websites
   map.resources   :users,           :password_resets
-  
+
   map.with_options :controller => 'home' do |home|
     home.dashboard 'dashboard', :action => 'dashboard'
   end
-  
+
   # End point route
-  map.root :controller => "home", :action => "index" 
+  map.root :controller => "home", :action => "index"
 end
