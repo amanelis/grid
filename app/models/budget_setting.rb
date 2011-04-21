523e09779db1f37ea7644a5747a436d73aa06cee
markleem <mark.mayfield@cityvoice.com>
2011-04-21 15:39:00 -0500

    Added secondary sort field for named scopes to ensure that the most recently changed object of a group with a similar start date is returned last.

--- app/models/budget_setting.rb
+++ app/models/budget_setting.rb
@@ -1,7 +1,7 @@
 class BudgetSetting < ActiveRecord::Base
   belongs_to :channel
   
-  named_scope :upto, lambda { |date| {:conditions => ['start_date <= ?', date], :order => 'start_date ASC'} }
+  named_scope :upto, lambda { |date| {:conditions => ['start_date <= ?', date], :order => 'start_date ASC, updated_at ASC'} }
   
   validates_numericality_of :amount, :greater_than_or_equal_to => 0.0, :message => "must be an amount $0.00 or greater"
   validates_presence_of :start_date
--- app/models/rake_setting.rb
+++ app/models/rake_setting.rb
@@ -1,7 +1,7 @@
 class RakeSetting < ActiveRecord::Base
   belongs_to :channel
   
-  named_scope :upto, lambda { |date| {:conditions => ['start_date <= ?', date], :order => 'start_date ASC'} }
+  named_scope :upto, lambda { |date| {:conditions => ['start_date <= ?', date], :order => 'start_date ASC, updated_at ASC'} }
   
   validates_numericality_of :percentage, :only_integer => true, :greater_than_or_equal_to => 0, :message => "must be an integer zero or greater"
   validates_presence_of :start_date
