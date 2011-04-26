0539ccc4f9b8c88aad74093ce7d7a5f821457019
Matthew Johnston <warmwaffles@gmail.com>
2011-04-26 09:36:17 -0500

    Removed inline html from strings. Fixed the flash error / notice jittering.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,16 @@
 class ApplicationController < ActionController::Base
   #include SslRequirement
   include ExceptionNotification::Notifiable
-  
+
   helper :all # include all helpers, all the time
   #protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
-  
+
   #CanCan rescue errors and access denied
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = "<span class=\"error-box\">Ooops, looks like you do not have permissions to view that page!</span>"
+    flash[:error] = "Ooops, looks like you do not have permissions to view that page!"
     redirect_to dashboard_url
   end
 
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
           if f.to_s == "html"
             r.nil? ? format.html : format.html {redirect_to r}
           elsif f.to_s == "js"
-            r.nil? ? format.js : format.js {redirect_to r} 
+            r.nil? ? format.js : format.js {redirect_to r}
           elsif f.to_s == "xml"
             r.nil? ? format.xml : format.xml {render :xml => r}
           elsif f.to_s == "json"
@@ -36,13 +36,13 @@ class ApplicationController < ActionController::Base
         end
       end
     end
-    
+
     def datepicker(*args)
       if params[:to].blank? || params[:from].blank?
         @start_date = Date.today - 1.month
         @end_date = Date.today
       else
-        begin 
+        begin
           @start_date = Date.parse(params[:from])
           @end_date = Date.parse(params[:to])
         rescue
@@ -50,32 +50,32 @@ class ApplicationController < ActionController::Base
           @end_date = Date.yesterday
           flash[:error] = "The date you entered was incorrect, we set it back to <strong>#{(@start_date).to_s(:medium)} to #{@end_date.to_s(:medium)}</strong> for you."
           respond("html", args.first)
-        end 
+        end
       end
     end
-    
+
     def require_ssl
-      redirect_to :protocol => "https://" unless (request.ssl? or local_request?)  
+      redirect_to :protocol => "https://" unless (request.ssl? or local_request?)
     end
-    
+
     # resource is refereing to the restful functions that require an instance variable of the controller name
     def load_time_zone
       Time.zone = resource.time_zone
     end
-    
+
     def no_layout
       render :layout => false
     end
-    
+
     def check_authorization
       redirect_to root_url unless current_user
     end
-    
+
     def load_resource_user
-      @user = current_user 
+      @user = current_user
       @current_user = current_user
     end
-  
+
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
       @current_user_session = UserSession.find
@@ -85,7 +85,7 @@ class ApplicationController < ActionController::Base
       return @current_user if defined?(@current_user)
       @current_user = current_user_session && current_user_session.user
     end
-  
+
     def require_user
       unless current_user
         store_location
@@ -103,7 +103,7 @@ class ApplicationController < ActionController::Base
         return false
       end
     end
-    
+
     def require_admin
       unless current_user && current_user.admin?
         flash[:notice] = "You are not authorized to access this page."
@@ -111,11 +111,11 @@ class ApplicationController < ActionController::Base
         return false
       end
     end
-    
+
     def store_location
       session[:return_to] = request.request_uri
     end
-    
+
     def redirect_back_or_default(default)
       redirect_to(session[:return_to] || default)
       session[:return_to] = nil
